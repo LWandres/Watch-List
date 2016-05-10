32945e677da28ae468b3468163d4402178d6da22
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-05-10 15:25:04 -0700

    Initial Commit

---
+++ Bucket List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bucket List
+//
+//  Created by Howard Jiang on 5/10/16.
+//  Copyright © 2016 Howard Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
